dd2b3b9fa5a0e8efe4848323069ed09229a233bd
Lee Winder <lee.winder@gmail.com>
2025-09-19 10:56:45 +0100

    ...

--- scripts/00 - Common/logging_utils.py
+++ scripts/00 - Common/logging_utils.py
@@ -21,7 +21,7 @@ def setup_pipeline_logging(
 
     logging.basicConfig(
         level=level,
-        format='%(asctime)s - %(levelname)-8s - %(message)s',
+        format='%(asctime)s - %(levelname)-8s  %(message)s',
         datefmt='%Y-%m-%d %H:%M:%S',
         handlers=[
             logging.FileHandler(log_file, mode='a'),  # Append to shared file
